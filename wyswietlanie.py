742533c7d1b0d32d5eecd847d96ce0aa8036534e
i359317 <i359317@uwr.edu.pl>
2026-01-03 23:55:09 +0100

    Rozwinąłem funkcjonalność menu przy zaczynaniu nowej gry, dodałem wybór poziomów trudności i zacząłem tworzyć zegar mierzący czas gry

--- wyswietlanie.py
+++ wyswietlanie.py
@@ -1,26 +1,53 @@
+import time
 import curses
 from curses import wrapper
+
+def rozgrywka(stdscr, poziom):
+
+    zegar = curses.newwin(1, 20, 0, 0) #zegar odliczajacy sekundy
+    zegar.refresh()
+
+    stdscr.erase()
+    stdscr.refresh()
+ 
+    czas = 0
+    start = time.time()
+    while True:             #tutaj trzeba pracowac nad wyswietlaniem planszy
+        zegar.erase()
+
+        zegar.addstr(f"Time: {czas}")
+        zegar.refresh()
+
+        if time.time() - start >= 1:
+            czas = czas + 1
+            start = time.time()
+        
 
 def menu_glowne(stdscr):
     curses.curs_set(0)  # niech kursor sie nie wyswietla
     curses.start_color()  # dodaj kolory
     
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
+
+    stdscr.nodelay(True) #nie czeka na nacisniecie klawisza, dzieki czemu inne funkcje dzieja sie w tle
     
     menu = ['Nowa Gra', 'Wczytaj Gre', 'Zasady Gry', 'Wyjscie']
+    poziomy = ['Latwy  ★☆☆☆☆ ', 'Sredni ★★★☆☆ ', 'Trudny ★★★★★ ']
+    tytuly = ["--- SAPER ---", "--- POZIOMY ---"]
+    ekran = [menu, poziomy]
+    iterator = 0
     obecny_rzad = 0
 
     while True:
-        stdscr.clear()
+        stdscr.erase()
         
         wysokosc, szerokosc = stdscr.getmaxyx() # pobiera informacje o wysokosci i szerokosci okna terminala
         
-        tytul = "--- SAPER ---"
-        stdscr.addstr(wysokosc // 2 - len(menu) // 2 - 2, szerokosc // 2 - len(tytul) // 2, tytul, curses.A_BOLD)
+        stdscr.addstr(wysokosc // 2 - len(ekran[iterator]) // 2 - 2, szerokosc // 2 - len(tytuly[iterator]) // 2, tytuly[iterator], curses.A_BOLD)
 
-        for indeks, rzad in enumerate(menu):
+        for indeks, rzad in enumerate(ekran[iterator]):
             x = szerokosc // 2 - len(rzad) // 2
-            y = wysokosc // 2 - len(menu) // 2 + indeks
+            y = wysokosc // 2 - len(ekran[iterator]) // 2 + indeks  # w curses nie da sie chyba ustawic napisu na wspolrzednej float, wiec jest krzywo
             
             if indeks == obecny_rzad:
                 stdscr.attron(curses.color_pair(1))
@@ -31,22 +58,39 @@ def menu_glowne(stdscr):
 
         stdscr.refresh()
 
-        klawisz = stdscr.getkey()
+        try:            # jesli uzytkownik nic nie klika to klawisz = None, aby nie crashowalo
+            klawisz = stdscr.getkey()
+        except:
+            klawisz = None
 
         if klawisz == 'KEY_UP':
-            obecny_rzad = (obecny_rzad - 1) % len(menu)
+            obecny_rzad = (obecny_rzad - 1) % len(ekran[iterator])
             
         elif klawisz == 'KEY_DOWN':
-            obecny_rzad = (obecny_rzad + 1) % len(menu)
+            obecny_rzad = (obecny_rzad + 1) % len(ekran[iterator])
+
+        elif klawisz == 'q': #cofnij - dokonczyc, gdy zrobimy zasady gry oraz logowanie
+            if iterator == 1:
+                stdscr.clear()
+                iterator = 0
             
         elif klawisz == '\n':
-            wybrana_opcja = menu[obecny_rzad]
+            wybrana_opcja = ekran[iterator][obecny_rzad]
             
             if wybrana_opcja == 'Wyjscie':
                 break
             elif wybrana_opcja == 'Nowa Gra':
                 stdscr.clear()
-                stdscr.addstr(0, 0, "Tu uruchomi sie gra... Nacisnij cos by wrocic.")
-                stdscr.getch()
+                iterator = 1
+            elif wybrana_opcja == 'Wczytaj Gre':
+                pass #wczytaj zapamietana gdzies plansze
+            elif wybrana_opcja == 'Zasady Gry':
+                pass #Zrobic nowego windowa lub pada na zasady, ktore trzeba w README.md uzupelnic
+            elif wybrana_opcja == poziomy[0]:
+                rozgrywka(stdscr, 'latwy')
+            elif wybrana_opcja == poziomy[1]:
+                rozgrywka(stdscr, 'sredni')
+            elif wybrana_opcja == poziomy[2]:
+                rozgrywka(stdscr, 'trudny')
 
 wrapper(menu_glowne)
