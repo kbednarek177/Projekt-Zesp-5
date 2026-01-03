88427a3aa5203ab1dfe763d1f9528875ca5c86e6
jantar07 <zolzuzanna45@gmail.com>
2026-01-03 20:51:59 +0100

    Dodałam więcej komentarzy. Dodałam tablicę słowników w pliku json jako definitywny sposób zapisu.

--- konta.py
+++ konta.py
@@ -1,33 +1,34 @@
+
+## Plik z danymi składa się z listy L słowników (w każdym kluczami są nazwy użyt.), gdzie L[0] - hasła, L[1] - najlepsze wyniki itd.
+
 from pathlib import Path ## chat gpt mi pomogl z odpowiednim dopasowaniem sciezki
 import hashlib           ## tutaj również chat mi podpowiedział żeby haszować hasła za pomocą tej funkcji
 import json 
-directory = Path(__file__).resolve().parent   ##- chat gpt
 
-dane = directory / "test.json"                 ##- chat gpt
 
-ostatnie_gra = directory / "test2.txt"
+
 
 
 def wczytaj_dane(dane):
     with open(dane,  mode="r", encoding="utf-8") as dane:
         d = json.load(dane)
-        nazwy_hasla = json.loads(d)
-    return nazwy_hasla
+        d1 = json.loads(d)
+    return d1
         
 
-##funkcje zwiazane z tworzeniem konta
+##funkcje zwiazane z tworzeniem konta i logowaniem (uzywa L[0])
 
-def hasz(a: str) -> str:                                    ## jak wyzej, stwierdzilam ze zrobie to faktycznie haszowaniem
+def hasz(a: str) -> str:                                    ## patrz import hashlib
     return hashlib.sha256(a.encode("utf-8")).hexdigest()    ##- chat gpt
 
-def czy_wolna(nazwa, nazwy_hasla): ## nazwy użytkownika nie mogą się powtarzać!
+def czy_wolna(nazwa, nazwy_hasla): ## nazwy użytkownika nie mogą się powtarzać
     for x in nazwy_hasla.keys():
         if(x == nazwa):
             return False
     return True
 
 
-def zaloz_konto(nazwy_hasla):    ## pierwsza poważna funkcjonalność - zakładanie konta. do pliku dodaję nowe dane na samym końcu. 
+def zaloz_konto(nazwy_hasla):    ## zakładanie konta. do pliku dodaję nowe dane na samym końcu. 
     nazwa = input()
     
     while(czy_wolna(nazwa,nazwy_hasla)==False):
@@ -57,8 +58,15 @@ def zaloguj(nazwa, haslo, nazwy_hasla):
 
 ##funkcje zwiazane z nadpisywaniem pliku z danymi
 
-def nadpisz_plik(nazwy_hasla, dane):
-    nowy = json.dumps(nazwy_hasla)
-    print(nowy)
+def nadpisz_plik(nazwy_hasla, nazwy_wynik, dane):
+    dane_do_zapisu = []
+    dane_do_zapisu.append(nazwy_hasla)
+    dane_do_zapisu.append(nazwy_wynik)
+    zapis = json.dumps(dane_do_zapisu)
     with open(dane, mode="w", encoding="utf-8") as d:
-        json.dump(nowy, d)
+        json.dump(zapis, d)
+
+
+directory = Path(__file__).resolve().parent   ##- chat gpt
+
+dane = directory / "zapisane_dane.json"       ##- chat gpt
