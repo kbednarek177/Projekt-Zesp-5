0f4dfc2eba902d2d6804a7a4d4f0467155aa8e47
jantar07 <zolzuzanna45@gmail.com>
2026-01-03 21:49:16 +0100

    dodałam wczytywanie/nadpisywanie pliku z zapisem, logowanie, tworzenie konta, dodatkowo załączam znowu podsumowanie.tex zrobione przez Agatkę
    * Ułożyłam szablon pliku podsumowanie.tex
    
    * dodalam sensowne wczytywanie danych, zakladanie konta i sprawdzanie hasel
    
    * poprawilam sposob zapisywania danych
    
    * Zmieniłam typ zapisywania nazw i haseł na .json. Usunęłam plik zapisane_dane.txt i zastąpiłam go zapisane_dane.json
    
    * Dodałam więcej komentarzy. Dodałam tablicę słowników w pliku json jako definitywny sposób zapisu.
    
    * Dodałam przypadek gdzie plik z zapisem jest pusty.
    
    ---------
    
    Co-authored-by: i359408 <359408@uwr.edu.pl>

---
+++ konta.py
@@ -0,0 +1,79 @@
+
+## Plik z danymi składa się z listy L słowników (w każdym kluczami są nazwy użyt.), gdzie L[0] - hasła, L[1] - najlepsze wyniki itd.
+
+from pathlib import Path ## chat gpt mi pomogl z odpowiednim dopasowaniem sciezki
+import hashlib           ## tutaj również chat mi podpowiedział żeby haszować hasła za pomocą tej funkcji
+import json 
+
+
+
+
+
+def wczytaj_dane(dane):
+    with open(dane,  mode="r", encoding="utf-8") as dane:
+        d = json.load(dane)
+        d1 = json.loads(d)
+    return d1
+        
+
+##funkcje zwiazane z tworzeniem konta i logowaniem (uzywa L[0])
+
+def hasz(a: str) -> str:                                    ## patrz import hashlib
+    return hashlib.sha256(a.encode("utf-8")).hexdigest()    ##- chat gpt
+
+def czy_wolna(nazwa, nazwy_hasla): ## nazwy użytkownika nie mogą się powtarzać
+    for x in nazwy_hasla.keys():
+        if(x == nazwa):
+            return False
+    return True
+
+
+def zaloz_konto(nazwy_hasla):    ## zakładanie konta. do pliku dodaję nowe dane na samym końcu. 
+    nazwa = input()
+    
+    while(czy_wolna(nazwa,nazwy_hasla)==False):
+        print("Nazwa zajęta!")
+        nazwa = input()
+
+    haslo_nieszyfr = input()
+    haslo = hasz(haslo_nieszyfr)
+    
+    nazwy_hasla[nazwa] = haslo
+
+    return nazwa
+
+
+def sprawdz_haslo(nazwa, haslo, nazwy_hasla):   ## sprawdzanie hasla 
+    haslo_shaszowane = hasz(haslo)
+    if(nazwy_hasla[nazwa] == haslo_shaszowane):
+        return True
+    return False
+
+
+def zaloguj(nazwa, haslo, nazwy_hasla):
+    if(sprawdz_haslo(nazwa, haslo, nazwy_hasla) == True):
+        return True
+    return False
+        
+
+##funkcje zwiazane z nadpisywaniem pliku z danymi
+
+def nadpisz_plik(nazwy_hasla, nazwy_wynik, dane):
+    dane_do_zapisu = []
+    dane_do_zapisu.append(nazwy_hasla)
+    dane_do_zapisu.append(nazwy_wynik)
+    zapis = json.dumps(dane_do_zapisu)
+    with open(dane, mode="w", encoding="utf-8") as d:
+        json.dump(zapis, d)
+
+
+directory = Path(__file__).resolve().parent   ##- chat gpt
+
+dane = directory / "zapisane_dane.json"       ##- chat gpt
+
+if(dane.stat().st_size != 0):
+    tablica_dane = wczytaj_dane(dane)
+else:
+    tablica_dane = [dict(), dict()]
+
+
