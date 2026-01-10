f06d37a89ea6ef0981a845d82c67d4ffb9d76607
JakubChytron <kubach06@o2.pl>
2026-01-10 20:40:29 +0100

    Przepisanie z planszy do sapera
    Przerzuciłem moją pracę z pliku plansza w branchu generowanie do pliku saper. Przy okazji zrobiłem kilka drobnych zmian aby ułatwić złączenie z wcześniej napisanym kodem.

--- saper.py
+++ saper.py
@@ -1,20 +1,48 @@
-
+from random import randint
 class Plansza:
 
     def __init__(self, szer, wys, bomby):
-        self.szer = szer
-        self.wys = wys
+        self.szerokosc = szer
+        self.wysokosc = wys
         self.bomby = bomby
-        self.plansza = [[0 for i in range(szer)] for j in range(wys)]  # plansza[wiersz][kolumna]
+        self.tablica = [[0 for i in range(szer)] for j in range(wys)]  # tablica[wiersz][kolumna]
         self.wyswietlana = [[9 for i in range(szer)] for j in range(wys)]
 
 def generowanie(plansza):
-    pass
-        #funkcja która generuje planszę(tablicę) o wymiarach plansza.wys i plansza.szer, która ma plansza.bomby bomb
-        #funkcja zmienia plansza.plansza
-        
+    # wypełnianie planszy bombami
+    for k in range(plansza.ilosc_bomb):
+        sel_x = randint(0, plansza.szerokosc-1)
+        sel_y = randint(0, plansza.wysokosc-1)
+        # rerollujemy wybór z tablicy aż trafimy na pole bez bomby
+        while(plansza.tablica[sel_y][sel_x] == -1):
+            sel_x = randint(0, plansza.szerokosc-1)
+            sel_y = randint(0, plansza.wysokosc-1)
+        plansza.tablica[sel_y][sel_x] = -1
+        # i oraz j dają nam wszystkich sąsiadów pola, aby je uaktualnić
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                # nie chcemy wyjść poza tablicę
+                if(sel_x+i < 0 or sel_x+i >= plansza.wysokosc or sel_y+j < 0 or sel_y+j >= plansza.szerokosc):
+                    continue
+                # nie chcemy nadpisać min
+                if(plansza.tablica[sel_y+j][sel_x+i] > -1):
+                    plansza.tablica[sel_y+j][sel_x+i] += 1
+    #funkcja która generuje planszę(tablicę) o wymiarach plansza.wys i plansza.szer, która ma plansza.bomby bomb
+    #funkcja zmienia plansza.tablica
+def wygrana(plansza):
+    #placeholder
+    return False
 def ruch(pozycja, plansza):
-    x, y = pozycja #y - wiersz, x- kolumna
+    y, x = pozycja
+    plansza.wyswietlana[y][x] = 1
+    if(plansza.tablica[y][x] == -1):
+        return 2
+    if(plansza.wyswietlana[y][x] != 10):
+        plansza.wyswietlana[y][x] = plansza.tablica[y][x]
+    if(wygrana(plansza)):
+        return 1
+    return 0
+    #y - wiersz, x- kolumna
         #funkcja który sprawdza, czy można odkryć element, odkrywa i sprawdza czy partia jest wygrana/przegrana
         #funkcja zmienia plansza.wyswietlana
         
